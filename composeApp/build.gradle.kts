15300c9a9cc19efd856e2146c011640ab311812c
kikepb7 <enriquepalmadev@gmail.com>
2025-10-22 23:34:26 +0200

    PROJECT | Define module relations

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,6 +32,22 @@ kotlin {
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
+            // CORE
+            implementation(projects.core.data)
+            implementation(projects.core.domain)
+            implementation(projects.core.designsystem)
+            implementation(projects.core.presentation)
+
+            // AUTH FEATURE
+            implementation(projects.feature.auth.domain)
+            implementation(projects.feature.auth.presentation)
+
+            // CHAT FEATURE
+            implementation(projects.feature.chat.data)
+            implementation(projects.feature.chat.database)
+            implementation(projects.feature.chat.domain)
+            implementation(projects.feature.chat.presentation)
+
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
@@ -47,9 +63,6 @@ kotlin {
         }
         iosMain.dependencies {
 
-        }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
         }
     }
 }
