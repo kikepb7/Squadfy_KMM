30620028332fd2fd5779b7952041cec6a161489d
kikepb7 <enriquepalmadev@gmail.com>
2025-11-05 00:00:46 +0100

    LOCALHOST | Include url and config to use localhost backend

--- core/data/src/commonMain/kotlin/com/kikepb/core/data/networking/UrlConstants.kt
+++ core/data/src/commonMain/kotlin/com/kikepb/core/data/networking/UrlConstants.kt
@@ -2,5 +2,5 @@ package com.kikepb.core.data.networking
 
 object UrlConstants {
 //    const val BASE_URL_HTTP = "https:/squadfy.com/api"
-    const val BASE_URL_HTTP = "http://localhost:8080/api"
+    const val BASE_URL_HTTP = "http://10.0.2.2:8080/api"    // Emulator
 }
