630449fac09fb796b105805b937f31bc78b07fc0
kikepb7 <enriquepalmadev@gmail.com>
2025-11-15 01:56:37 +0100

    EXPIRED SESSIONS | Implement handling expired sessions

--- composeApp/src/commonMain/kotlin/org/kikepb/squadfy/App.kt
+++ composeApp/src/commonMain/kotlin/org/kikepb/squadfy/App.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.rememberNavController
 import com.kikepb.auth.presentation.navigation.AuthGraphRoutes.Graph
 import com.kikepb.chat.presentation.chat_list.ChatListRoute
 import com.kikepb.core.designsystem.theme.SquadfyTheme
+import com.kikepb.core.presentation.util.ObserveAsEvents
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.kikepb.squadfy.navigation.DeepLinkListener
 import org.kikepb.squadfy.navigation.NavigationRoot
@@ -24,6 +25,18 @@ fun App(
 
     LaunchedEffect(key1 = state.isCheckingAuth) {
         if (!state.isCheckingAuth) onAuthenticationChecked()
+    }
+
+    ObserveAsEvents(flow = viewModel.events) { event ->
+        when (event) {
+            is MainEvent.OnSessionExpired -> {
+                navController.navigate(route = Graph) {
+                    popUpTo(Graph) {
+                        inclusive = false
+                    }
+                }
+            }
+        }
     }
 
     SquadfyTheme {
--- composeApp/src/commonMain/kotlin/org/kikepb/squadfy/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/kikepb/squadfy/MainViewModel.kt
@@ -3,18 +3,41 @@ package org.kikepb.squadfy
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kikepb.core.domain.auth.repository.SessionStorage
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.kikepb.squadfy.MainEvent.OnSessionExpired
 
 class MainViewModel(
     private val sessionStorage: SessionStorage
 ) : ViewModel() {
 
+    private var previousRefreshToken: String? = null
+    private var hasLoadedInitialData = false
+    private val eventChannel = Channel<MainEvent>()
+    val events = eventChannel.receiveAsFlow()
     private val _state = MutableStateFlow(MainState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart {
+            if (!hasLoadedInitialData) {
+                observeSession()
+                hasLoadedInitialData = true
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = MainState()
+        )
+
 
     init {
         viewModelScope.launch {
@@ -23,9 +46,30 @@ class MainViewModel(
             _state.update { it.copy(isCheckingAuth = false, isLoggedIn = authInfo != null) }
         }
     }
+
+    private fun observeSession() {
+        sessionStorage
+            .observeAuthInfo()
+            .onEach { authInfo ->
+                val currentRefreshToken = authInfo?.refreshToken
+                val isSessionExpired = previousRefreshToken != null && currentRefreshToken == null
+
+                if (isSessionExpired) sessionStorage.set(info = null)
+                _state.update { it.copy(isLoggedIn = false) }
+
+                eventChannel.send(OnSessionExpired)
+
+                previousRefreshToken = authInfo?.refreshToken
+            }
+            .launchIn(viewModelScope)
+    }
 }
 
 data class MainState(
     val isLoggedIn: Boolean = false,
     val isCheckingAuth: Boolean = true
 )
+
+sealed interface MainEvent {
+    data object OnSessionExpired: MainEvent
+}
